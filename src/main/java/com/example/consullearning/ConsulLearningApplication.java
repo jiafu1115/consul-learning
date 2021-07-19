41984709699a35ac80f644afa40148282bafa38c
Stroller Fu <stroller.fu@zoom.us>
2021-07-19 14:25:09 +0800

    add producer

--- src/main/java/com/example/consullearning/ConsulLearningApplication.java
+++ src/main/java/com/example/consullearning/ConsulLearningApplication.java
@@ -2,11 +2,20 @@ package com.example.consullearning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ConsulLearningApplication {
 
-    public static void main(String[] args) {
+
+   @RequestMapping("/health")
+   public String home() {
+        return "Hello world";
+   }
+
+   public static void main(String[] args) {
         SpringApplication.run(ConsulLearningApplication.class, args);
     }
 

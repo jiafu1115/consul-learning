55a5ca88194691850234ab83bb4013e6d4438c2b
Stroller Fu <stroller.fu@zoom.us>
2021-07-19 11:13:44 +0800

    Initial commit

---
+++ src/main/java/com/example/consullearning/ConsulLearningApplication.java
@@ -0,0 +1,13 @@
+package com.example.consullearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsulLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsulLearningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/consullearning/ConsulLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.consullearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsulLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
